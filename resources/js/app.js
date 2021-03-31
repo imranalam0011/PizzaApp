a7b322060b0e773ebeb792c6eddac068139ae28e
Imran <imranlm677@gmail.com>
2021-04-01 01:47:24 +0530

    cart funtion dynamically working

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
-console.log("hello from app js");
+import axios from 'axios';
+import Noty from 'noty';
+
+let addTocart = document.querySelectorAll('.add-to-cart')
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(pizza) {
+    axios.post('/update-cart', pizza).then(res => {
+        cartCounter.innerText = res.data.totalQty
+
+        // Alert when new item will add
+        new Noty({
+            type: 'success',
+            timeout: 1000,
+            progressBar: false,
+            text: 'Item added to cart'
+        }).show();
+    }).catch(err => {
+        new Noty({
+            type: 'error',
+            timeout: 1000,
+            progressBar: false,
+            text: 'Something went wrong'
+        }).show();
+    })
+}
+
+addTocart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        // let pizza = btn.dataset.pizza
+        let pizza = JSON.parse(btn.dataset.pizza)
+        updateCart(pizza)
+
+    })
+})
